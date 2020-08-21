212c23234af72f7207fa9a0b8a61577112de5869
jhke01 <jiahong.ke@inl.gov>
2020-08-20 22:17:46 -0600

    add sink, recombination, trapping terms

--- xolotl/xolotlCore/reactants/uzrclusters/UZrVCluster.h
+++ xolotl/xolotlCore/reactants/uzrclusters/UZrVCluster.h
@@ -20,6 +20,17 @@ private:
 		return nameStream.str();
 	}
 
+	/**
+	 * The sink strength
+	 */
+	double sinkStrength;
+
+	/**
+	 * The recombination term
+	 */
+	double recombStrength;
+	double trapStrength;
+
 public:
 
 	/**
@@ -47,7 +58,33 @@ public:
 
 		// TODO: Compute the reaction radius
 		// You can look at FeVCluster or PSIVCluster
-		reactionRadius = network.getLatticeParameter();
+		//reactionRadius = network.getLatticeParameter();
+
+		// Compute the reaction radius
+		double latticeParam = network.getLatticeParameter();
+		// It is the same formula for HeV clusters
+		reactionRadius = latticeParam
+				* pow((3.0 * size) / xolotlCore::pi, (1.0 / 3.0)) * 0.5;
+
+		//! Parameters for biased sink in the iron case
+		double r0 = latticeParam * 0.75 * sqrt(2.0);
+		double reactionRadius = latticeParam * cbrt(3.0 / xolotlCore::pi) * 0.5;
+		constexpr double rho = 1e14 * 1e-18; //sink density? in a unit of nm^-2?
+
+		sinkStrength = rho * 1.00;
+		/*
+		sinkStrength = -4.0 * xolotlCore::pi * rho
+				/ log(
+						xolotlCore::pi * rho * (reactionRadius + r0)
+								* (reactionRadius + r0));
+		*/
+
+		recombStrength = 4.0 * xolotlCore::pi * reactionRadius * 1.00;
+
+
+
+		trapStrength = 4.0 * xolotlCore::pi * reactionRadius * 0.00;
+
 
 		return;
 	}
@@ -59,6 +96,70 @@ public:
 
 	//! Destructor
 	~UZrVCluster() {
+	}
+
+	/**
+	 * This operation returns the total change in this cluster due its
+	 * own dissociation.
+	 *
+	 * @param i The location on the grid in the depth direction
+	 * @return The flux due to its dissociation
+	 */
+	double getEmissionFlux(int i) override {
+		// Initial declarations
+		double flux = UZrCluster::getEmissionFlux(i);
+		double k_b = xolotlCore::kBoltzmann;
+		double temp = network.getTemperature();
+		//double D_v = 1e-4*exp(-2.0/k_b/temp)*1e18; //D in nm^2/s
+		double Di = 1e-8*exp(-0.6/k_b/temp)*1e18; //D in nm^2/s
+
+		auto singleXeCluster = network.get(Species::Xe, 1);
+		auto conc = singleXeCluster->getConcentration();
+		//std::cout << "conc " << conc << std::endl;
+
+		// Compute the loss to dislocation sinks
+		if (size < 2) {
+			// k^2 * D * C
+			flux += sinkStrength * diffusionCoefficient[i] * concentration
+					+ recombStrength * (Di + diffusionCoefficient[i])
+						* diffusionCoefficient[i] / Di * concentration * concentration
+					+ trapStrength * diffusionCoefficient[i] * concentration * conc;
+		}
+
+		return flux;
+	}
+
+	/**
+	 * This operation computes the partial derivatives due to emission
+	 * reactions.
+	 *
+	 * @param partials The vector into which the partial derivatives should be
+	 * inserted. This vector should have a length equal to the size of the
+	 * network.
+	 * @param i The location on the grid in the depth direction
+	 */
+	void getEmissionPartialDerivatives(std::vector<double> & partials,
+			int i) const override {
+		// Initial declarations
+		UZrCluster::getEmissionPartialDerivatives(partials, i);
+		double k_b = xolotlCore::kBoltzmann;
+		double temp = network.getTemperature();
+		//double D_v = 1e-4*exp(-2.0/k_b/temp)*1e18; //D in nm^2/s
+		double Di = 1e-8*exp(-0.6/k_b/temp)*1e18; //D in nm^2/s
+
+		auto singleXeCluster = network.get(Species::Xe, 1);
+		auto conc = singleXeCluster->getConcentration();
+
+		// Compute the loss to dislocation sinks
+		if (size < 2) {
+			// k^2 * D * C
+			partials[id - 1] -= sinkStrength * diffusionCoefficient[i]
+			+	recombStrength * (Di + diffusionCoefficient[i])
+				* diffusionCoefficient[i] / Di * concentration * 2
+			+ trapStrength * diffusionCoefficient[i] * conc;
+		}
+
+		return;
 	}
 
 	/**
